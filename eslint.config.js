b0453ce0a07cf4fa31954ab80acfbbfcfd8b2e10
th0h0e <thmasthe@gmail.com>
2026-02-08 04:28:20 +0100

    Exclude VUE_MIGRATION_PLAN.md and src/components from linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
+  ignores: ['VUE_MIGRATION_PLAN.md', 'src/components/**'],
   rules: {
     'react/no-array-index-key': 'off',
   },
