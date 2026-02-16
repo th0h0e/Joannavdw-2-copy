c14a0f4816e05a963d429429833ecfb4c9bc04e4
th0h0e <thmasthe@gmail.com>
2026-02-16 22:23:01 +0100

    feat: add monochrome theme and admin form font-size fix

--- layers/2.admin/app/layouts/admin.vue
+++ layers/2.admin/app/layouts/admin.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="bg-default min-h-screen">
+  <div class="admin-layout bg-default min-h-screen">
     <slot />
   </div>
 </template>
+
+<style scoped>
+.admin-layout {
+  font-size: 16px;
+}
+
+.admin-layout :deep(input),
+.admin-layout :deep(textarea),
+.admin-layout :deep(select) {
+  font-size: 16px;
+}
+</style>
